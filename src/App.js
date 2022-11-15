54ce6557bc473b35ac068692684236111a08469f
Pedro Machado <pedro.machado@liferay.com>
2022-11-15 17:05:04 -0300

    feat: change button disabled status on checkbox click

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 
 function App() {
   const [toggleColor, setToggleColor] = useState(false);
+  const [toggleDisabled, setToggleDisabled] = useState(false);
 
   return (
     <div>
       <button
         style={{ backgroundColor: toggleColor ? "blue" : "red" }}
         onClick={() => setToggleColor(!toggleColor)}
+        disabled={toggleDisabled}
       >
         Change to {toggleColor ? "red" : "blue"}
       </button>
+      <input
+        type="checkbox"
+        onClick={() => setToggleDisabled(!toggleDisabled)}
+      />
     </div>
   );
 }
